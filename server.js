96d567ced7fd0d46026f2cefdcf86d3a038a7786
Chip Cantrell <charles.cantrell@me.com>
2015-03-03 14:33:52 -0700

    Postman works for login and register

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./api/models/userModel');
+var userCtrl = require('./api/controllers/userCtrl');
 var port = 8080;
 
 mongoose.connect('mongodb://localhost/pinkHippos');
 app.use(bodyParser.json());
 app.use(express.static(__dirname+'/public'));
+app.use(session({
+
+	secret: 'PINKhippos'
+
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(new LocalStrategy({
 
@@ -21,21 +29,25 @@ passport.use(new LocalStrategy({
 
 	User.findOne({ email: username }).exec().then(function(user) {
 
+		debugger;
+
 		console.log('User found with: ', user);
 
 		if (!user) {
 		
 			return done(null, false);
 		
-		}
+		};
 		
 		user.comparePassword(password).then(function(isMatch) {
+
+			console.log('isMatch: ', isMatch);
 		
 			if (!isMatch) {
 		
 				return done(null, false);
 		
-			}
+			};
 		
 			return done(null, user);
 		
@@ -59,30 +71,10 @@ passport.deserializeUser(function(obj, done) {
 
 app.post('/api/login', passport.authenticate('local'), function(req, res) {
 
-	console.log(req.user);
-
 	res.status(200).json(req.user);
 
 });
 
-app.post('/api/register', function(req, res) {
-
-	console.log(req.body);
-
-	var newUser = new User(req.body);
-
-	newUser.save(function(err, user) {
-
-		if (err) {
-
-			return res.status(500).end();
-
-		};
-
-		return res.json(user);
-
-	})
-
-});
+app.post('/api/register', userCtrl.registerUser);
 
 app.listen(port)
